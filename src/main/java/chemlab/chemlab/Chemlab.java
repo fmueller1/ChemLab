6d8334f6c2587e3d18abc9cbc2ad2d79fcc266f6
Felix <felixmmueller06@gmail.com>
2026-04-24 01:02:14 +0400

    call ModItems init method

--- src/main/java/chemlab/chemlab/Chemlab.java
+++ src/main/java/chemlab/chemlab/Chemlab.java
@@ -18,7 +18,7 @@ public class Chemlab implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+        ModItems.initialize();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
